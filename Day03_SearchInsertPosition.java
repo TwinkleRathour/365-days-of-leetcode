f38f77a7fd505b1621ad14df79ef039d50595d93
Twinkle Rathour <twinklerathour88@gmail.com>
2026-03-28 21:36:10 +0530

    Search Insert Position

---
+++ Day03_SearchInsertPosition.java
@@ -0,0 +1,30 @@
+/**
+ * LeetCode 35 - Search Insert Position
+ *
+ * Given a sorted array of distinct integers and a target value,
+ * return the index if the target is found. If not, return the index
+ * where it would be if it were inserted in order.
+ *
+ * Time Complexity:  O(log n) - Binary Search
+ * Space Complexity: O(1)
+ */
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+}
